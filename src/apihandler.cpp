3e0f22eaa67e9f60d8c004347873c2e993a5c734
Aleksandr Dovydenkov <asd@altlinux.org>
2026-04-19 13:34:15 +0300

    Add simple api handler

---
+++ src/apihandler.cpp
@@ -0,0 +1,145 @@
+#include <boost/json/object.hpp>
+#include <boost/json/parse.hpp>
+#include <boost/json/serialize.hpp>
+#include <cgicc/Cgicc.h>
+#include <cgicc/HTTPResponseHeader.h>
+#include <cgicc/HTTPStatusHeader.h>
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <unordered_map>
+
+#include "apihandler.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+std::string extract_bearer_token(const std::string &auth_header) {
+    if (auth_header.substr(0, 7) == "Bearer ") {
+        std::string token = auth_header.substr(7);
+        // Use your token here
+        auto l = token.find_first_not_of(" \t\r\n");
+        auto r = token.find_last_not_of(" \t\r\n");
+        if (l == std::string::npos)
+          return {};
+        return token.substr(l, r - l + 1);
+    }
+    return {};
+}
+
+Request ApiHandler::request() {
+  try {
+    cgicc::Cgicc cgi;
+    const char* t = std::getenv("HTTP_AUTHORIZATION");
+    token = (t != nullptr) ? extract_bearer_token(t) : "";
+
+    auto content_length = cgi.getEnvironment().getContentLength();
+    string body;
+    if (content_length == 0 || content_length > 10'000) {
+      send_error(431);
+      return HANDLED_ERROR;
+    }
+
+    body.resize(content_length);
+    cin.read(&body[0], content_length);
+
+    boost::json::error_code ec;
+    boost::json::value jv = boost::json::parse(body, ec);
+    if (ec) {
+      send_error(418);
+      return HANDLED_ERROR;
+    }
+
+    if (!jv.is_object()) {
+      send_error(418);
+      return HANDLED_ERROR;
+    }
+    req = jv.as_object();
+  } catch (const std::exception &e) {
+    send_error(500);
+    return HANDLED_ERROR;
+  }
+}
+
+string ApiHandler::get(string key) { return req[key].as_string().c_str(); }
+
+size_t ApiHandler::get_int(string key) { return req[key].as_uint64(); }
+
+size_t ApiHandler::get_route_index() { return route_index; }
+
+string ApiHandler::get_token() { return token; }
+
+string reason_for(int code) {
+  static const std::unordered_map<int, string> reason_map = {
+      {100, "Continue"},
+      {101, "Switching Protocols"},
+      {102, "Processing"},
+      {200, "OK"},
+      {201, "Created"},
+      {202, "Accepted"},
+      {203, "Non-Authoritative Information"},
+      {204, "No Content"},
+      {205, "Reset Content"},
+      {206, "Partial Content"},
+      {300, "Multiple Choices"},
+      {301, "Moved Permanently"},
+      {302, "Found"},
+      {303, "See Other"},
+      {304, "Not Modified"},
+      {305, "Use Proxy"},
+      {307, "Temporary Redirect"},
+      {308, "Permanent Redirect"},
+      {400, "Bad Request"},
+      {401, "Unauthorized"},
+      {402, "Payment Required"},
+      {403, "Forbidden"},
+      {404, "Not Found"},
+      {405, "Method Not Allowed"},
+      {406, "Not Acceptable"},
+      {407, "Proxy Authentication Required"},
+      {408, "Request Timeout"},
+      {409, "Conflict"},
+      {410, "Gone"},
+      {411, "Length Required"},
+      {412, "Precondition Failed"},
+      {413, "Payload Too Large"},
+      {414, "URI Too Long"},
+      {415, "Unsupported Media Type"},
+      {416, "Range Not Satisfiable"},
+      {417, "Expectation Failed"},
+      {418, "I’m a teapot"},
+      {426, "Upgrade Required"},
+      {431, "Request Header Fields Too Large"},
+      {500, "Internal Server Error"},
+      {501, "Not Implemented"},
+      {502, "Bad Gateway"},
+      {503, "Service Unavailable"},
+      {504, "Gateway Timeout"},
+      {505, "HTTP Version Not Supported"}};
+
+  auto it = reason_map.find(code);
+  return (it != reason_map.end()) ? it->second : string("Status");
+}
+
+void ApiHandler::send(boost::json::object response, size_t err) {
+  if (responsed) {
+    return;
+  }
+
+  cout << cgicc::HTTPStatusHeader(err, reason_for(err)) << endl;
+  cout << "Content-Type: application/json\r\n\r\n";
+  cout << boost::json::serialize(response) << endl;
+  responsed = true;
+}
+
+void ApiHandler::send_error(size_t err) {
+  if (responsed) {
+    return;
+  }
+
+  boost::json::object resp;
+  resp["ok"] = false;
+  send(resp, err);
+}
---
+++ src/apihandler.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <boost/json/object.hpp>
+#include <string>
+
+using std::string;
+
+enum Request {
+  LOGIN,
+  ME,
+  TASKS,
+  TASK_BY_ID,
+  REPORTS,
+  ADD_REPORT,
+  REPORTS_BY_ID,
+  GRADE_REPORT,
+  HANDLED_ERROR,
+};
+
+class ApiHandler {
+private:
+    string token;
+    boost::json::object req;
+    bool responsed = false;
+    size_t route_index = 0;
+
+public:
+  Request request();
+
+  string get(string key);
+  size_t get_int(string key);
+  size_t get_route_index();
+  string get_token();
+
+  void send(boost::json::object response, size_t err=200);
+  void send_error(size_t err);
+};
